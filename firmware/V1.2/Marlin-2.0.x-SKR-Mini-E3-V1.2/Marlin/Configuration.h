3b5702a93e0b67ba09b4eac87d20bc933e441952
Rostislav Petr <rostislav.petr@gmail.com>
2020-10-01 13:34:49 +0200

    base compilation

--- firmware/V1.2/Marlin-2.0.x-SKR-Mini-E3-V1.2/Marlin/Configuration.h
+++ firmware/V1.2/Marlin-2.0.x-SKR-Mini-E3-V1.2/Marlin/Configuration.h
@@ -132,7 +132,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Toy Factory F"
+#define CUSTOM_MACHINE_NAME "Toy Factory DIRECT"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like http://www.uuidgenerator.net/version4
